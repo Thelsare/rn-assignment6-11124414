80f4178bd87124a85992e1f37bb8b58fefdf24b2
THELMA ASARE <thelmaasare66@gmail.com>
2024-07-03 14:13:38 -0700

    MyWebsite Update

--- MyWebsite/App.js
+++ MyWebsite/App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import CartScreen from './CartScreen';
+import AppLoading from 'expo-app-loading';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home"  screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Checkout" component={CartScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
